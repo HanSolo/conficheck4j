a56bfce2fbc204fdffffcdf8de646007f940cb43
Gerrit Grunwald <han.solo@mac.com>
2025-03-18 11:03:56 +0100

    bumped to 21.0.11

--- build_app_windows.bat
+++ build_app_windows.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 set JAVA_VERSION=21
-set MAIN_JAR=conficheck4j-21.0.9.jar
-set APP_VERSION=21.0.9
+set MAIN_JAR=conficheck4j-21.0.11.jar
+set APP_VERSION=21.0.11
 
 rem ------ SETUP DIRECTORIES AND FILES ----------------------------------------
 rem Remove previously generated java runtime and installers. Copy all required
